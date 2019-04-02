9e5bd73a2e59cbbf84a433ace4e3088cf976c95b
dererumnatura <pollindrom@gmail.com>
2019-04-02 14:06:40 +0300

    before Vlad fixes

--- assets/hiphop.js
+++ assets/hiphop.js
@@ -1,18 +1,38 @@
+function dd(el) {
+    console.log(el);
+}
+
 let hiphopDude = document.getElementById('hiphop-dude');
 let playlist = document.getElementById('playlist');
 
 const playlistHeight = playlist.offsetHeight;
+const bottomHeightOfDude = window.innerHeight - hiphopDude.offsetHeight;
+
+let leftSideTitleBlock = document.getElementById('leftSideTitleBlock');
+leftSideTitleBlock.style.height = hiphopDude.offsetHeight - 8 + "px";
+
+let musicList = document.getElementById('musicList');
+musicList.style.paddingTop = window.innerHeight - hiphopDude.offsetHeight + "px";
 
 window.onscroll = function (e) {
-    if (playlist.getBoundingClientRect().bottom <= 362) {
-        hiphopDude.style.position = 'absolute';
-        hiphopDude.style.top = playlistHeight - 300 + 'px';
+    setTimeout(function () {
+        nextPosition = document.documentElement.scrollTop;
+    }, 100);
+
+    if (hiphopDude.getBoundingClientRect().bottom >= document.querySelector('.music-list li:last-child').getBoundingClientRect().bottom
+        && nextPosition < document.documentElement.scrollTop
+    ) {
+        hiphopDude.style.position = 'relative';
+        hiphopDude.style.display = 'flex';
+        hiphopDude.style.alignItems = 'flex-end';
     }
-    else {
+
+    if (bottomHeightOfDude + 200 <= musicList.getBoundingClientRect().bottom && nextPosition > document.documentElement.scrollTop) {
         hiphopDude.style.position = 'fixed';
-        hiphopDude.style.width = '50vw';
-        hiphopDude.style.right = '0';
-        hiphopDude.style.top = '10vh';
+        hiphopDude.style.right = 0;
+        // hiphopDude.style.bottom = 0;
     }
 }
 
+dd(window.innerHeight);
+dd(musicList.getBoundingClientRect().bottom);
